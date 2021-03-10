4100111537fb3ce535d5f3e436b7c0559b44f8c6
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2021-03-10 14:24:18 +0100

    Delete text_to_speech.ps1

--- text_to_speech.ps1
+++
@@ -1,6 +0,0 @@
-﻿Write-Host "text in file: "
-$items = Get-Content E:\users\bowen\onedrive\linkz\+\VBScript\newfile.txt
-$items | ForEach-Object {
-    echo $_
-}
-Write-Host "------------------------------------"
